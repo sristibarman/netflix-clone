47bce390c6bff6e5e25e0ba31ce8c199b66294b1
Sristi <sristibarman23gmail.com>
2023-04-19 15:18:07 +0530

    first commit

---
+++ netflix-api/server.js
@@ -0,0 +1,45 @@
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// mongoose.connect("mongodb://localhost:27017",{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+// }).then(()=>{
+//     console.log("db connected")
+// });
+
+// app.listen(5000,console.log("server started"));
+
+
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
---
+++ netflix-ui/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "409915e2b9ae8da336b708dfd2c2b029";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
